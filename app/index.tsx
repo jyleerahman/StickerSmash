5a726666430ac54203b8bbcaec642d4fb76a1d79
JY Lee <jylee.rahman@gmail.com>
2025-11-10 15:55:53 -0500

    Import stylesheet and create custom styles

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,20 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>👋</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>👋</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    color: "#fff"
+  }
+})
